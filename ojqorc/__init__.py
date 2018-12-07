576aa749f110f03fbf7d1223e2c025cfcf79783f
ojqorc <ojqorc@foxmail.com>
2018-12-07 11:08:39 +0000

    ojqorc

---
+++ ojqorc/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import wizard
+
+#class training_subject(Models.model):
+  #  _name='training.subject'
+   # _rec_name='ojqorc'
+ #   name=fields.Char(u'名称',sie=64,default=u'aaa')
+
+#  vim:et:si:sta:ts=4:sts=4:sw=4:tw=79:
